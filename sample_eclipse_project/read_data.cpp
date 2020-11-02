26e68ecedb6cd0dfcf10f10f1fe1dca28dfa836f
Christos Gogos <chgogos@gmail.com>
2020-11-02 22:31:57 +0200

    toy problem

---
+++ sample_eclipse_project/read_data.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <set>
+
+using namespace std;
+
+void read_data(string fn, int students, int exams) {
+	vector<set<int>> exam_students(exams + 1);
+	fstream fs(fn);
+	if (!fs.is_open()) {
+		cerr << "Could not open file " << fn << std::endl;
+		exit(-1);
+	}
+	int student_id = 0;
+	string line;
+	while (getline(fs, line)) {
+		if (line.empty())
+			continue;
+		student_id++;
+		istringstream iss(line);
+		int exam_id;
+		while (iss >> exam_id) {
+			exam_students[exam_id].insert(student_id);
+			cout << "Student " << student_id << " is enrolled in exam "
+					<< exam_id << endl;
+		}
+	}
+	fs.close();
+
+	cout << "#################################" << endl;
+
+	for (int i = 1; i <= exams; i++) {
+		cout << "Exam " << i << " Enrolled students: ";
+		for (int student_id : exam_students[i]) // range based for
+		{
+			cout << student_id << " ";
+		}
+		cout << endl;
+	}
+}
+
+int main() {
+	read_data("car-f-92.stu", 18419, 543);
+	read_data("toy_e5_s6.stu", 6, 5);
+}
